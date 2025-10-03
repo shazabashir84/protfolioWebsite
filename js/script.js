c8a6ea1381750427479d778c9a9c53dfef0b0bf3
Shaza Salah <shazabashir84@gmail.com>
2025-10-03 16:52:55 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function animateCounter(selector, end, duration) {
+    const el = document.querySelector(selector);
+    let start = 0;
+    const stepTime = Math.abs(Math.floor(duration / end));
+    const timer = setInterval(() => {
+        start++;
+        el.textContent = start;
+        if (start >= end) clearInterval(timer);
+    }, stepTime);
+}
+
+// Animate members counter to 200 over 2 seconds
+animateCounter('.members', 200, 2000);
+animateCounter('.projects',345,2000);
+animateCounter('.downloads',1800,2000);
+animateCounter('.codeLines',1200,2000);
