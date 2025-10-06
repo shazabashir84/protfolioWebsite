8edbbedb51902b12bc248a433b632275f675fe0c
Shaza Salah <shazabashir84@gmail.com>
2025-10-06 22:21:26 +0300

    complete

--- js/script.js
+++ js/script.js
@@ -24,3 +24,57 @@ animateCounter('.members', 200, 2000);
 animateCounter('.projects',345,2000);
 animateCounter('.downloads',1800,2000);
 animateCounter('.codeLines',1200,2000);
+// 
+// 
+// carousel section using swiper js
+ new Swiper('.swiper', {
+  // Optional parameters
+//   direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+});
+// end carousel
+// 
+// 
+// ---------------------------------------
+// section four filter images by class
+const filterButtons = document.querySelectorAll(".filter-btn");
+const galleryItems = document.querySelectorAll(".gallery");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    filterButtons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+
+    const filterValue = button.dataset.filter;
+    console.log("filterValue:", filterValue)
+
+    galleryItems.forEach((item) => {
+      if (filterValue === "all" || item.classList.contains(filterValue)) {
+        item.classList.remove("hidden");
+      } else {
+        item.classList.add("hidden");
+      }
+    });
+  });
+});
